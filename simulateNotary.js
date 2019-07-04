24ee6fb6404b4b96a6ea4818c2e2433b6be0a265
Doron Orenstein <jazziseverywhere@gmail.com>
2019-07-04 15:16:58 -0500

    more work, still not working

--- simulateNotary.js
+++ simulateNotary.js
@@ -2,36 +2,42 @@
 // Will be changed to require ('FactomSDK') after publish
 const configure = require("./configure");
 
-var Twit = require('twit')
+var Twit = require("promised-twit");
 
-var T = new Twit(configure)
+var T = new Twit(configure);
 
 // Handle node response
 const responseData = (response, data) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(data), "utf-8");
+    console.log('YO', response);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(data), "utf-8");
 };
 
-module.exports = async (request, response) => {
-  // Init factom sdk with your appId and appKey, which can be found or generated at https://account.factom.com
+module.exports =  (request, response) => {
+    // Init factom sdk with your appId and appKey, which can be found or generated at https://account.factom.com
 
-  try {
-    // Create identity without keys. System automatic generating 3 key pairs.
+    // try {
+        // Create identity without keys. System automatic generating 3 key pairs.
 
-     const statuses = await T.get('statuses/user_timeline', { screen_name: 'realDonaldTrump', count: 10 }, function(err, data, response) {
-        // console.log('data', data)
-        return data.map(function (val) {
-            console.log('TWEET TWEET!:  ', val.text);
-            return val.text;
-        });
-      });
+        return  T.get(
+            "statuses/user_timeline",
+            { screen_name: "realDonaldTrump", count: 10 },
+            function(err, data, response) {
+
+                // console.log('data', data)
+                return data.map(function(val) {
+                    console.log("TWEET TWEET!:  ", val.text);
+                    responseData(response, {
+                        statuses: response
+                    });
+
+                    // return val.text;
+                });
+            }
+        );
 
 
-    responseData(response, {
-        testStuff: 'DORON',
-        statuses: statuses
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 };
